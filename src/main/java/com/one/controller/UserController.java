d5c0a71e56fb28fb929dbc058165d1e45995b11f
LonelyLeo <1061154936@qq.com>
2020-04-21 08:58:11 +0800

    搭建环境

---
+++ src/main/java/com/one/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.one.controller;
+
+import com.one.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+}
---
+++ src/main/java/com/one/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.one.service.impl;
+
+import com.one.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
